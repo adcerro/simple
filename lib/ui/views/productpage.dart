53d347aa7bd563a227220db5498eae6b05e59e1b
adcerro <adcerro@uninorte.edu.co>
2024-11-30 02:51:00 -0500

    filler text

--- lib/ui/views/productpage.dart
+++ lib/ui/views/productpage.dart
@@ -19,7 +19,9 @@ class ProductPage extends StatelessWidget {
               Text(
                 product,
                 style: Theme.of(context).textTheme.titleLarge,
-              )
+              ),
+              const Text(
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mollis nibh id nisl semper bibendum a sit amet sem. Fusce sit amet nibh semper neque accumsan vehicula at non eros. Fusce id sem felis. Vestibulum purus massa, ornare sed purus non, ultricies dapibus orci. Sed ullamcorper facilisis pretium. Integer eros sem, facilisis ut fermentum vitae, mattis sed diam. Proin nec erat leo. In hac habitasse platea dictumst. Integer accumsan nunc nisi.")
             ],
           ),
         ));
